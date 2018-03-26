5f13bfb67909598bc84b98f1e8e457cfb350f9f3
Mark Fingerhuth <markfingerhuth@protonmail.com>
2018-03-25 22:44:32 -0400

    new test_ising_mock tests in order to test new way of calculating n_nodes

--- grove/tests/ising/test_ising.py
+++ grove/tests/ising/test_ising.py
@@ -37,3 +37,45 @@ def test_ising_mock():
 
     assert most_freq_string_ising == [-1, -1, 1, -1]
     assert energy_ising == -9
+
+    with patch("pyquil.api.QVMConnection") as cxn:
+        # Mock the response
+        cxn.run_and_measure.return_value = [[1, 0, 1, 0]]
+        cxn.expectation.return_value = [0, 0, 0, 0] # dummy
+
+    # checkerboard
+    J = {(0, 1): 1, (0, 2): 1, (1, 3): 1, (2, 3): 1}
+    h = {}
+    p = 1
+    most_freq_string_ising, energy_ising, circuit = ising(h, J, num_steps=p, vqe_option=None, connection=cxn)
+
+    assert most_freq_string_ising == [-1, 1, -1, 1]
+    assert energy_ising == 0
+
+    with patch("pyquil.api.QVMConnection") as cxn:
+        # Mock the response
+        cxn.run_and_measure.return_value = [[1, 0, 1, 0]]
+        cxn.expectation.return_value = [0, 0, 0, 0] # dummy
+
+    # checkerboard
+    J = {}
+    h = {0: 1, 1: -1, 2: 1, 3: -1}
+    p = 1
+    most_freq_string_ising, energy_ising, circuit = ising(h, J, num_steps=p, vqe_option=None, connection=cxn)
+
+    assert most_freq_string_ising == [-1, 1, -1, 1]
+    assert energy_ising == -4
+
+    with patch("pyquil.api.QVMConnection") as cxn:
+        # Mock the response
+        cxn.run_and_measure.return_value = [[1, 0, 1, 0, 1]]
+        cxn.expectation.return_value = [0, 0, 0, 0, 0] # dummy
+
+    # checkerboard
+    J = {(0, 4): -1}
+    h = {0: 1, 1: -1, 2: 1, 3: -1}
+    p = 1
+    most_freq_string_ising, energy_ising, circuit = ising(h, J, num_steps=p, vqe_option=None, connection=cxn)
+
+    assert most_freq_string_ising == [-1, 1, -1, 1, -1]
+    assert energy_ising == -5
